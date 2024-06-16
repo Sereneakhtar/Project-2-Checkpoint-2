889bf6459cef8cadf823c7ba11b3260eb42d22fe
Sereneakhtar <158603230+Sereneakhtar@users.noreply.github.com>
2024-06-15 20:39:10 -0700

    Add files via upload

---
+++ util.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "util.h"
+#include "heap.h"
+
+void handleInstructions(HEAP* heap, int flag, const char* inputFile, const char* outputFile) {
+    // Implementation of instruction handling
+    // This includes reading from input, executing commands, and writing to output
+}
---
+++ util.h
@@ -0,0 +1,8 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include "data_structures.h"
+
+void handleInstructions(HEAP* heap, int flag, const char* inputFile, const char* outputFile);
+
+#endif
